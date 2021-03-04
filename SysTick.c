3ad3134ff7dde3c56622ba54de45c0de6b25b49d
chloetang28 <73394953+chloetang28@users.noreply.github.com>
2021-03-04 14:43:17 -0600

    Add files via upload

---
+++ SysTick.c
@@ -0,0 +1,36 @@
+// SysTick.h
+// Runs on TM4C123
+// Provide functions that initialize the SysTick module, wait at least a
+// designated number of clock cycles, and wait approximately a multiple
+// of 10 milliseconds using busy wait. 
+// Daniel Valvano
+// January 17, 2021
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+/* 
+ Copyright 2021 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// Initialize SysTick with busy wait running at bus clock.
+void SysTick_Init(void){
+  // write this
+}
+// The delay parameter is in units of the 80 MHz core clock. (12.5 ns)
+void SysTick_Wait(uint32_t delay){
+  // write this
+}
+// 10000us equals 10ms
+void SysTick_Wait10ms(uint32_t delay){
+  // write this
+}
+
